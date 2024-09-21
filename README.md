31428bba70099f12a8b3a9fc81c631b1e67dabf3
dotschmann <73004827+dotschmann@users.noreply.github.com>
2024-09-21 17:25:09 +0200

    how the calculatation for getting the correct angle expected
    created a function using turtle_module for produce a isosceles triangle

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # turtle_module_shape
 Using turtle module to create different shapes example polygons and flowers
+
+## equilateral triangle
+base length, side length have the same value and internal angles are the same.
+if you use the left-return (.lt), you use the external angle (180-60) of the input
+Hence it is easy to use the for_range method to lop through 3 times to get the triangle.
+
+## isosceles triangle
+use the desired angle(θ) and base-length (b) to calculate the side-length (s) of the triangle.
+ s = b / (2 * cos(θ))
--- triangle.py
+++ triangle.py
@@ -2,14 +2,35 @@ import turtle
 
 poly = turtle.Turtle()
 
+#Isosceles triangle with base angle 54°, base length of 100 and side length of 85
+
+def isosceles_tri():
+    poly.fd(100)
+    poly.lt(180-54)
+
+    poly.fd(85)
+    poly.lt(180)
+
+    poly.fd(85)
+    poly.lt(180+54)
+
+    poly.fd(100)
+    poly.lt(180+54)
+
+    poly.fd(85)
+
+
+
+isosceles_tri()
+
 # equilateral triangle
 
-def equi_angle():
-    for _ in range(3):
-        poly.fd(100)
-        poly.lt(120)
+# def equi_angle():
+#     for _ in range(3):
+#         poly.fd(100)
+#         poly.lt(120)
 
     
-equi_angle()
+# equi_angle()
 
 turtle.mainloop()
