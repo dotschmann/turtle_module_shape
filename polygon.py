9eeca12008bbbe067eade37981b8d7e4c2a3634d
dotschmann <73004827+dotschmann@users.noreply.github.com>
2024-09-21 22:03:05 +0200

    created hexagon

--- polygon.py
+++ polygon.py
@@ -5,4 +5,21 @@ poly = turtle.Turtle()
 
 
 
+def polygon():
+
+    poly.fd(100)
+    poly.lt(60)
+    poly.fd(100)
+    poly.lt(60)
+    poly.fd(100)
+    poly.lt(60)
+    poly.fd(100)
+    poly.lt(60)
+    poly.fd(100)
+    poly.lt(60)
+    poly.fd(100)
+
+
+polygon()
+
 turtle.mainloop()
