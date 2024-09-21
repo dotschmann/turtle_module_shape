a8e153fe8919a060153c0bf72ac8300fb8a9fcab
dotschmann <73004827+dotschmann@users.noreply.github.com>
2024-09-21 22:19:20 +0200

    created general set of functions(polygon and create_polygon for polygons

--- polygon.py
+++ polygon.py
@@ -5,13 +5,23 @@ poly = turtle.Turtle()
 
 
 
-def polygon():
-    for i in range(6):
-        poly.fd(100)
-        poly.lt(60)
+def polygon(t):
+    number_of_side = 9
+    total_internal_angle = (number_of_side-2) * 180
+    per_angle = int (total_internal_angle/number_of_side)
+    length = 100
+
+    create_polygon(t,number_of_side, per_angle, length)
 
 
 
-polygon()
+def create_polygon(t, n, a, l):
+    for i in range(n):
+        t.fd(l)
+        t.lt(180 - a)
+
+
+
+polygon(poly)
 
 turtle.mainloop()
